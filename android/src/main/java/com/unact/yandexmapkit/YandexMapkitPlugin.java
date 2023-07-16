897a4e6c1d262f17eb4d4345462ed339538c0e0f
Uladzislau Zbarouski <vladzborovsky@gmail.com>
2023-07-16 20:17:48 +0500

    Refactor package to mapkit_lite

--- android/src/main/java/com/unact/yandexmapkit/YandexMapkitPlugin.java
+++ android/src/main/java/com/unact/yandexmapkit/YandexMapkitPlugin.java
@@ -16,17 +16,8 @@ import io.flutter.plugin.common.MethodChannel;
 
 public class YandexMapkitPlugin implements FlutterPlugin, ActivityAware {
   private static final String VIEW_TYPE = "yandex_mapkit/yandex_map";
-  private static final String SEARCH_CHANNEL_ID   = "yandex_mapkit/yandex_search";
-  private static final String SUGGEST_CHANNEL_ID  = "yandex_mapkit/yandex_suggest";
-  private static final String DRIVING_CHANNEL_ID  = "yandex_mapkit/yandex_driving";
-  private static final String BICYCLE_CHANNEL_ID  = "yandex_mapkit/yandex_bicycle";
 
   @Nullable private Lifecycle lifecycle;
-
-  @Nullable private MethodChannel searchMethodChannel;
-  @Nullable private MethodChannel suggestMethodChannel;
-  @Nullable private MethodChannel drivingMethodChannel;
-  @Nullable private MethodChannel bicycleMethodChannel;
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
@@ -34,47 +25,10 @@ public class YandexMapkitPlugin implements FlutterPlugin, ActivityAware {
 
     BinaryMessenger messenger = binding.getBinaryMessenger();
     binding.getPlatformViewRegistry().registerViewFactory(VIEW_TYPE, new YandexMapFactory(messenger, new LifecycleProvider()));
-
-    setupChannels(messenger, binding.getApplicationContext());
   }
 
   @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    teardownChannels();
-  }
-
-  private void setupChannels(BinaryMessenger messenger, Context context) {
-    searchMethodChannel = new MethodChannel(messenger, SEARCH_CHANNEL_ID);
-    YandexSearch yandexSearch = new YandexSearch(context, messenger);
-    searchMethodChannel.setMethodCallHandler(yandexSearch);
-
-    suggestMethodChannel = new MethodChannel(messenger, SUGGEST_CHANNEL_ID);
-    YandexSuggest yandexSuggest = new YandexSuggest(context, messenger);
-    suggestMethodChannel.setMethodCallHandler(yandexSuggest);
-
-    drivingMethodChannel = new MethodChannel(messenger, DRIVING_CHANNEL_ID);
-    YandexDriving yandexDriving = new YandexDriving(context, messenger);
-    drivingMethodChannel.setMethodCallHandler(yandexDriving);
-
-    bicycleMethodChannel = new MethodChannel(messenger, BICYCLE_CHANNEL_ID);
-    YandexBicycle yandexBicycle = new YandexBicycle(context, messenger);
-    bicycleMethodChannel.setMethodCallHandler(yandexBicycle);
-  }
-
-  @SuppressWarnings({"ConstantConditions"})
-  private void teardownChannels() {
-    searchMethodChannel.setMethodCallHandler(null);
-    searchMethodChannel = null;
-
-    suggestMethodChannel.setMethodCallHandler(null);
-    suggestMethodChannel = null;
-
-    drivingMethodChannel.setMethodCallHandler(null);
-    drivingMethodChannel = null;
-
-    bicycleMethodChannel.setMethodCallHandler(null);
-    bicycleMethodChannel = null;
-  }
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {}
 
   @Override
   public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
